d9f35f32503221d807454982abf18c1e7ba389d1
0x4kgi <17591127+0x4kgi@users.noreply.github.com>
2022-10-31 20:30:29 +0800

    initial commit, i guess

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+    import HelloWorld from "./lib/HelloWorld.svelte";
+    import Counter from "./lib/Counter.svelte";
+    import Declarations from "./lib/Declarations.svelte";
+</script>
+
+<main>
+    <HelloWorld />
+
+    <p><Counter /></p>
+
+    <p><Declarations /></p>
+
+    <p />
+</main>
+
+<style>
+    main {
+        text-align: center;
+        margin: auto;
+        font-size: larger;
+    }
+</style>
