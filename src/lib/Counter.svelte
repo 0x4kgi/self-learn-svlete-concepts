4802a5866c2cd83ec95c8ad57c3781f6e1f6c320
0x4kgi <17591127+0x4kgi@users.noreply.github.com>
2022-10-31 20:33:54 +0800

    why did the other files did not commit, wtf

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,11 @@
+<script>
+    let count = 0;
+
+    function incrementCounter() {
+        count += 1;
+    }
+</script>
+
+<button on:click={incrementCounter}>
+    The count is: {count}
+</button>
---
+++ src/lib/Declarations.svelte
@@ -0,0 +1,16 @@
+<script>
+    let count = 0;
+
+    function incrementCounter() {
+        count += 1;
+    }
+
+    $: doubled = count * 2;
+    $: quad = count * 4;
+</script>
+
+<button on:click={incrementCounter}>
+    The count is: {count}
+</button>
+<p>Doubled: {doubled}</p>
+<p>Quad: {quad}</p>
